b61f5d73c00b7d619502cffa32b27d65c735af6c
ERCIN <ercinakkaya@ERCINs-Laptop.local>
2025-01-01 22:58:41 +0300

    custom topbar was complated

---
+++ app/src/main/java/com/example/android_case_study/presentation/components/BaseTopBar.kt
@@ -0,0 +1,40 @@
+package com.example.android_case_study.presentation.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.android_case_study.databinding.BaseTopBarBinding
+
+class BaseTopBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private var binding: BaseTopBarBinding = BaseTopBarBinding.inflate(LayoutInflater.from(context), this)
+
+
+    fun setTitle(title: String) {
+        binding.titleTxt.text = title
+    }
+
+    fun setType(hasIcon: Boolean) {
+        if(hasIcon) {
+            binding.apply {
+                btnBack.visibility = VISIBLE
+            }
+        } else {
+            binding.apply {
+                btnBack.visibility = GONE
+            }
+        }
+    }
+
+    fun setOnBackClickListener(listener: () -> Unit) {
+        binding.btnBack.setOnClickListener { listener.invoke() }
+    }
+
+}
