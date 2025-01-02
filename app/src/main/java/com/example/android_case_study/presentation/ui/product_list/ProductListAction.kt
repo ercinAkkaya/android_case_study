c559c3b93079b6d64d07896d7cc9d33a79aefb8e
ERCIN <ercinakkaya@ERCINs-Laptop.local>
2025-01-03 00:43:52 +0300

    some fix (in effect, action, list fragment)

--- app/src/main/java/com/example/android_case_study/presentation/ui/product_list/ProductListAction.kt
+++ app/src/main/java/com/example/android_case_study/presentation/ui/product_list/ProductListAction.kt
@@ -1,10 +1,11 @@
 package com.example.android_case_study.presentation.ui.product_list
 
-import com.example.android_case_study.core.base.UIAction
 
 sealed class ProductListAction {
     data class OnInputChange(val input: String) : ProductListAction()
     data class OnFilter(val minPrice: Double) : ProductListAction()
     data object ToggleBottomSheet : ProductListAction()
     data object Refresh : ProductListAction()
+    data class AddFavorite(val productName: String) : ProductListAction()
+    data class DeleteFavorite(val productName: String) : ProductListAction()
 }
